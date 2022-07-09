95fa9c65ac7c1095c48d1c621f9e0e3b46bd5136
dragunwf <plarisanmarc@gmail.com>
2022-07-09 21:59:41 +0800

    Implemented private player fields for future use and added the ability to jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int health = 3;
+    private const float jumpForce = 7.5f;
+    private Rigidbody2D rigidBody;
+
+    private void Awake()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+    }
+
+    private void OnJump()
+    {
+        rigidBody.velocity += new Vector2(0, jumpForce);
+        // Add sound effect in the future
+    }
+
+    private void OnFire()
+    {
+
     }
 }
