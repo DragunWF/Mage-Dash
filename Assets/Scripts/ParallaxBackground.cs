a134639f4d6a342943941b9a27b815473807cb14
dragunwf <plarisanmarc@gmail.com>
2022-07-09 19:50:02 +0800

    Implemented script for parallax background

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float Speed { get; private set; }
+
+    private float initialSpeedValue = 5f;
+    private float xPositionLimit = -14;
+    private float xResetPoint = 14;
+
+    public void UpdateSpeed(float newValue)
     {
-        
+        Speed = -newValue * Time.deltaTime;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        Speed = -initialSpeedValue * Time.deltaTime;
+    }
+
+    private void Update()
+    {
+        float xPosition = transform.position.x;
+
+        if (transform.position.x < xPositionLimit)
+            transform.position = new Vector2(xResetPoint, 0);
+        else
+            transform.Translate(Speed, 0, 0);
     }
 }
