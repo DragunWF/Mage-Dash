de357b77f645014f28ca4122e2864dbb358c7f29
dragunwf <plarisanmarc@gmail.com>
2022-07-13 19:01:35 +0800

    Implemented setup for dynamic obstacle spawning mechanic

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float[] spawnIntervals = new float[2] { 1.5f, 2.5f };
+    private bool spawnObstacles = true;
+
+    private GameObject[] groundObstacles;
+    private GameObject flyingObstacle;
+
+    private Transform flyingObstacleSpawnPoint;
+    private Transform groundObstacleSpawnPoint;
+
+    public void ScaleSpawner()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        flyingObstacle = Resources.Load("Prefabs/Obstacle [Bat]") as GameObject;
+
+        groundObstacleSpawnPoint = GameObject.Find("Ground Enemy Point").transform;
+        flyingObstacleSpawnPoint = GameObject.Find("Flying Enemy Point").transform;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SpawnObstacles());
+    }
+
+    private float GetRandomSpawnInterval()
+    {
+        float minSpawnInterval = spawnIntervals[0];
+        float maxSpawnInterval = spawnIntervals[1];
+
+        List<float> spawnIntervalsList = new List<float>();
+        float interval = minSpawnInterval;
+        while (interval < maxSpawnInterval)
+        {
+            spawnIntervalsList.Add(interval);
+            interval += 0.1f;
+        }
+
+        return spawnIntervalsList[Random.Range(0, spawnIntervalsList.Count)];
+    }
+
+    private GameObject GetRandomObstacle()
+    {
+        int chance = Random.Range(1, 2);
+        if (chance == 1)
+            return flyingObstacle;
+        return groundObstacles[Random.Range(0, groundObstacles.Length)];
+    }
+
+    private IEnumerator SpawnObstacles()
+    {
+        const float spawnDelay = 1.5f;
+        yield return new WaitForSeconds(spawnDelay);
+
+        while (spawnObstacles)
+        {
+            float spawnInterval = GetRandomSpawnInterval();
+            GameObject obstacle = GetRandomObstacle();
+            Transform spawnPosition = obstacle == flyingObstacle ?
+                flyingObstacleSpawnPoint : groundObstacleSpawnPoint;
+
+            Instantiate(obstacle, spawnPosition);
+            yield return new WaitForSeconds(spawnInterval);
+        }
     }
 }
