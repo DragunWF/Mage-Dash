1b4f9aa81942536735398efe9c9f15375e1af78d
dragunwf <plarisanmarc@gmail.com>
2022-10-14 17:17:33 +0800

    Added setup for functionality on difficulty scaling

--- Assets/Scripts/DifficultyScaling.cs
+++ Assets/Scripts/DifficultyScaling.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class DifficultyScaling : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int DifficultyLevel { get; private set; }
+    private float timeToScaleDifficulty = 10f;
+
+    private void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
     }
 }
