0a9e5b7de5207bf660cd1d496bb94f820f172474
dragunwf <plarisanmarc@gmail.com>
2022-10-29 01:04:46 +0800

    Updated ParallaxBackground.cs

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -6,24 +6,29 @@ public class ParallaxBackground : MonoBehaviour
 {
     public float Speed { get; private set; }
 
-    private float initialSpeedValue = 0.5f;
+    private float initialSpeedValue = 1.5f;
     private float xResetPoint = 14;
 
     public void UpdateSpeed(float newValue)
     {
-        Speed = -newValue * Time.deltaTime;
+        Speed = newValue;
     }
 
     private void Awake()
     {
-        Speed = -initialSpeedValue * Time.deltaTime;
+        Speed = initialSpeedValue;
     }
 
     private void Update()
     {
         if (transform.position.x < -xResetPoint)
+        {
             transform.position = new Vector2(xResetPoint, 0);
+        }
         else
-            transform.Translate(Speed, 0, 0);
+        {
+            float deltaSpeed = Speed * Time.deltaTime;
+            transform.Translate(-deltaSpeed, 0, 0);
+        }
     }
 }
