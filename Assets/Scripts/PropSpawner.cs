f0477be2e5d086ef286600a21454937c15f67ef1
dragunwf <plarisanmarc@gmail.com>
2022-10-29 02:19:17 +0800

    Implemented prop spawner's functionality

--- Assets/Scripts/PropSpawner.cs
+++ Assets/Scripts/PropSpawner.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PropSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float[] spawnIntervals = { 1.5f, 3.5f, 5.5f, 7.5f, 9.5f };
+    private Transform spawnPoint;
+    private GameObject prop;
+
+    private void Awake()
     {
-        
+        prop = Resources.Load("Prefabs/Cactus") as GameObject;
+        spawnPoint = GameObject.Find("Ground Enemy Point").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(SpawnProps());
+    }
+
+    private float GetRandomInterval()
+    {
+        return spawnIntervals[Random.Range(0, spawnIntervals.Length)];
+    }
+
+    private IEnumerator SpawnProps()
+    {
+        const float initialSpawnDelay = 1.5f;
+        yield return new WaitForSeconds(initialSpawnDelay);
+
+        while (true)
+        {
+            float interval = GetRandomInterval();
+            yield return new WaitForSeconds(interval);
+            Instantiate(prop, spawnPoint, true);
+            yield return new WaitForSeconds(interval);
+        }
     }
 }
