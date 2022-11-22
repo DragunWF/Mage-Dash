f4649421cb51a602364eda81b32f3b2a4385ded5
Marc Plarisan <plarisanmarc@gmail.com>
2022-11-22 14:13:42 +0800

    Created base template

--- Assets/Scripts/FadeToBlack.cs
+++ Assets/Scripts/FadeToBlack.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeToBlack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Image image;
+
+    public void Fade() 
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
     {
         
     }
--- Assets/Scripts/ParticlePlayer.cs
+++ Assets/Scripts/ParticlePlayer.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class ParticlePlayer : MonoBehaviour
 {
-    private void Start()
+    private GameObject playerDeathEffect;
+
+    public void PlayPlayerDeath(Vector2 pos)
     {
-        
+        Instantiate(playerDeathEffect, pos, Quaternion.identity);
+    }
+
+    private void Awake()
+    {
+        playerDeathEffect = Resources.Load("Prefabs/PlayerDeathEffect") as GameObject;
     }
 
     private void Update()
     {
-        
+
     }
 }
