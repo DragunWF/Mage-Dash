744fa4080a7fb47fca7bfe512a94d24c2e7e366e
dragunwf <plarisanmarc@gmail.com>
2022-11-25 00:42:31 +0800

    Implemented main UI functionality for retry menu

--- Assets/Scripts/RetryMenuUI.cs
+++ Assets/Scripts/RetryMenuUI.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public sealed class RetryMenuUI : MonoBehaviour
 {
-    private void Start()
+    private GameManager gameManager;
+    private GameStats gameStats;
+
+    private TextMeshProUGUI scoreText;
+    private TextMeshProUGUI highScoreText;
+    private GameObject newHighScoreText;
+
+    private void Awake()
     {
-        
+        gameManager = FindObjectOfType<GameManager>();
+        gameStats = FindObjectOfType<GameStats>();
+
+        scoreText = GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>();
+        highScoreText = GameObject.Find("HighScoreText").GetComponent<TextMeshProUGUI>();
+        newHighScoreText = GameObject.Find("NewHighScoreText");
     }
 
-    private void Update()
+    private void Start()
     {
-        
+        scoreText.text = string.Format("Score: {0}", gameStats.Score);
+        highScoreText.text = string.Format("High Score: {0}", gameStats.HighScore);
+        newHighScoreText.SetActive(gameStats.NewHighScore);
     }
 }
