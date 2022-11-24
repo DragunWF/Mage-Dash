52f498a5013fdaad2ecb22faaa1110d30f445326
Marc Plarisan <plarisanmarc@gmail.com>
2022-11-24 13:15:21 +0800

    Updated formatting GameUI

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -31,12 +32,14 @@ public class GameUI : MonoBehaviour
 
     public void UpdateScore(int newScore)
     {
-        scoreText.text = string.Format("Score: {0}", newScore);
+        string formattedScore = FormatScore(newScore);
+        scoreText.text = string.Format("Score: {0}", formattedScore);
     }
 
-    public void UpdateDifficulty(int newDifficulty)
+    public void UpdateDifficulty(int newDifficulty, bool maxDifficulty)
     {
-        difficultyText.text = string.Format("Difficulty: {0}", newDifficulty);
+        string levelDisplay = maxDifficulty ? "Max" : newDifficulty.ToString();
+        difficultyText.text = string.Format("Difficulty: {0}", levelDisplay);
     }
 
     private void Awake()
@@ -51,5 +54,28 @@ public class GameUI : MonoBehaviour
     {
         scoreText.text = "Score: 0";
         difficultyText.text = "Difficulty: 1";
+    }
+
+    private string FormatScore(int points)
+    {
+        if (points < 1000)
+        {
+            return points.ToString();
+        }
+
+        string formatted = "", str = points.ToString();
+        for (int i = 1, n = str.Length; i <= n; i++)
+        {
+            formatted += str[str.Length - i];
+            if (i + 1 <= n && i % 3 == 0)
+            {
+                formatted += ",";
+            }
+        }
+
+        char[] charArr = formatted.ToCharArray();
+        Array.Reverse(charArr);
+
+        return new string(charArr);
     }
 }
