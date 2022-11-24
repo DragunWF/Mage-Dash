25ad07f4426fff0ade8a7d4767828c68cf4ffffd
dragunwf <plarisanmarc@gmail.com>
2022-11-24 23:18:16 +0800

    Implemented fade to black transition

--- Assets/Scripts/FadeToBlack.cs
+++ Assets/Scripts/FadeToBlack.cs
@@ -5,20 +5,44 @@ using UnityEngine.UI;
 
 public sealed class FadeToBlack : MonoBehaviour
 {
-    private Image image;
+    private bool transitionInitialized;
+    private const float transitionDelay = 0.25f;
+    private Image blackScreen;
 
-    public void Fade() 
+    private float alphaSlider = 0;
+    private const float fadeSpeed = 145.5f;
+    private float loadSceneDelay = Mathf.Ceil(255 / fadeSpeed) + 1.5f;
+
+    public void InitializeFade()
     {
-        
+        Invoke("FadeScreen", transitionDelay);
+        StartCoroutine(CallLoadScene());
     }
 
     private void Awake()
     {
-        
+        blackScreen = GameObject.Find("ScreenFade").GetComponent<Image>();
+        blackScreen.gameObject.SetActive(false);
     }
 
     private void Update()
     {
-        
+        if (transitionInitialized)
+        {
+            alphaSlider += fadeSpeed * Time.deltaTime;
+            alphaSlider = Mathf.Clamp(alphaSlider, 0, 255);
+            blackScreen.color = new Color(0, 0, 0, alphaSlider / 255);
+        }
+    }
+
+    private void FadeScreen()
+    {
+        transitionInitialized = true;
+    }
+
+    private IEnumerator CallLoadScene()
+    {
+        yield return new WaitForSeconds(loadSceneDelay);
+        FindObjectOfType<GameManager>().LoadRetryMenu();
     }
 }
