df773757831f09ff8d31491cc3e2e50c011231a9
dragunwf <plarisanmarc@gmail.com>
2022-11-24 23:49:41 +0800

    Updated GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public sealed class GameManager : MonoBehaviour
 {
+    private const int gameSceneIndex = -1;
+    private const int retryMenuSceneIndex = -1;
+    private const int mainMenuSceneIndex = -1;
+    private const int shopMenuSceneIndex = -1;
+
+    public void LoadGameScene()
+    {
+        // load game scene
+    }
+
     public void LoadRetryMenu()
     {
         // load retry menu
+    }
+
+    public void LoadMainMenu()
+    {
+        // load main menu
+    }
+
+    public void LoadShopMenu()
+    {
+        // load shop menu
+    }
+
+    private void LoadScene()
+    {
+        // load scene
     }
 }
