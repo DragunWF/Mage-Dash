ec7c2697df1b02c20e5ca1991d82f057d887cf38
Marc Plarisan <plarisanmarc@gmail.com>
2022-11-26 17:16:32 +0800

    Updated MainMenuUI.cs

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public sealed class MainMenuUI : MonoBehaviour
 {
-    private void Start()
+    private GameObject mainMenuUI;
+    private GameObject tutorialUI;
+    private GameObject[] menuBackground;
+
+    public void OpenTutorial()
     {
-        
+        mainMenuUI.SetActive(false);
+        tutorialUI.SetActive(true);
     }
 
-    private void Update()
+    public void CloseTutorial()
     {
-        
+
+    }
+
+    private void Awake()
+    {
+        tutorialUI = GameObject.Find("HowToPlayUI");
+        tutorialUI.SetActive(false);
+
+        mainMenuUI = GameObject.Find("MainMenuUI");
+        mainMenuUI.SetActive(true);
+
+        menuBackground = new GameObject[3] {
+            GameObject.Find("World") as GameObject,
+            GameObject.Find("ObstacleSpawner") as GameObject,
+            GameObject.Find("PropSpawner") as GameObject,
+        };
     }
 }
