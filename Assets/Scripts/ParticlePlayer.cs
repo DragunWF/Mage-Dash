f6598ad865c4d8bb02e6662483d284d872a5ee39
Marc Plarisan <plarisanmarc@gmail.com>
2022-11-27 19:56:22 +0800

    Updated ParticlePlayer.cs

--- Assets/Scripts/ParticlePlayer.cs
+++ Assets/Scripts/ParticlePlayer.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public sealed class ParticlePlayer : MonoBehaviour
 {
     private GameObject playerDeathEffect;
+    private GameObject obstacleDeathEffect;
+    private GameObject fireballEffect;
 
-    public void PlayPlayerDeath(Vector2 pos)
+    public void PlayDeath(Vector2 position)
     {
-        Instantiate(playerDeathEffect, pos, Quaternion.identity);
+        SpawnParticle(playerDeathEffect, position);
+    }
+
+    public void PlayFireballHit(Vector2 position)
+    {
+        SpawnParticle(fireballEffect, position);
+    }
+
+    public void PlayObstacleDeath(Vector2 position)
+    {
+        SpawnParticle(obstacleDeathEffect, position);
     }
 
     private void Awake()
     {
         playerDeathEffect = Resources.Load("Prefabs/PlayerDeathEffect") as GameObject;
+        obstacleDeathEffect = Resources.Load("Prefabs/ObstacleDeathEffect") as GameObject;
+        fireballEffect = Resources.Load("Prefabs/FireballEffect") as GameObject;
     }
 
-    private void Update()
+    private void SpawnParticle(GameObject effect, Vector2 position)
     {
-
+        Instantiate(effect, position, Quaternion.identity);
     }
 }
