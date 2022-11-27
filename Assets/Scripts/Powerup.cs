97dda10d87aa6e51c94486e2834d0baa450f0464
Marc Plarisan <plarisanmarc@gmail.com>
2022-11-27 14:25:13 +0800

    Updated Powerup.cs

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public sealed class Powerup : MonoBehaviour
 {
-    private void Start()
+    [Header("Powerup Attributes")]
+    [SerializeField] string powerupType;
+
+    private const float speed = 2.5f;
+    private const float despawnPointX = -8f;
+
+    private void Awake()
     {
-        
+
     }
 
     private void Update()
     {
-        
+        float deltaSpeed = speed * Time.deltaTime;
+        transform.Translate(deltaSpeed, 0, 0);
+
+        if (transform.position.x <= despawnPointX)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // add future implementation here
+        }
     }
 }
