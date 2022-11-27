a721e72836cd79b76dc132cbb186c65268ae9b0a
Marc Plarisan <plarisanmarc@gmail.com>
2022-11-27 19:46:24 +0800

    Added base template for AudioPlayer.cs

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public sealed class AudioPlayer : MonoBehaviour
 {
-    private void Start()
+    private Vector2 defaultPosition = new Vector2(0, 0);
+    private AudioClip fireball;
+    private AudioClip death;
+    private AudioClip click; // add audio files
+
+    public void PlayFireball()
     {
-        
+        PlayClip(fireball, defaultPosition);
     }
 
-    private void Update()
+    public void PlayDeath()
     {
-        
+        PlayClip(death, defaultPosition);
+    }
+
+    public void PlayClick()
+    {
+        PlayClip(click, defaultPosition);
+    }
+
+    private void Awake()
+    {
+        fireball = Resources.Load("Audio/fireball") as AudioClip;
+        death = Resources.Load("Audio/death") as AudioClip;
+        click = Resources.Load("Audio/click") as AudioClip;
+    }
+
+    private void PlayClip(AudioClip clip, Vector2 position)
+    {
+
     }
 }
