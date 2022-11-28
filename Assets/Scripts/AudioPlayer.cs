e4dd175d88362ac06cc545d1e86a56075c97e7a3
Marc Plarisan <plarisanmarc@gmail.com>
2022-11-28 14:15:59 +0800

    Updated AudioPlayer.cs

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -4,35 +4,43 @@ using UnityEngine;
 
 public sealed class AudioPlayer : MonoBehaviour
 {
-    private Vector2 defaultPosition = new Vector2(0, 0);
-    private AudioClip fireball;
-    private AudioClip death;
-    private AudioClip click; // add audio files
+    private AudioClip shoot;
+    private const float shootVolume = 0.5f;
 
-    public void PlayFireball()
+    private AudioClip death;
+    private const float deathVolume = 0.75f;
+
+    private AudioClip click;
+    private const float clickVolume = 0.65f;
+
+    public void PlayShoot()
     {
-        PlayClip(fireball, defaultPosition);
+        PlayClip(shoot, shootVolume);
     }
 
     public void PlayDeath()
     {
-        PlayClip(death, defaultPosition);
+        PlayClip(death, deathVolume);
     }
 
     public void PlayClick()
     {
-        PlayClip(click, defaultPosition);
+        PlayClip(click, clickVolume);
     }
 
     private void Awake()
     {
-        fireball = Resources.Load("Audio/fireball") as AudioClip;
-        death = Resources.Load("Audio/death") as AudioClip;
-        click = Resources.Load("Audio/click") as AudioClip;
+        shoot = Resources.Load("Audio/Shoot") as AudioClip;
+        death = Resources.Load("Audio/Death") as AudioClip;
+        click = Resources.Load("Audio/Click") as AudioClip;
     }
 
-    private void PlayClip(AudioClip clip, Vector2 position)
+    private void PlayClip(AudioClip clip, float volume)
     {
-
+        if (clip != null)
+        {
+            Vector2 cameraPos = Camera.main.transform.position;
+            AudioSource.PlayClipAtPoint(clip, cameraPos, volume);
+        }
     }
 }
