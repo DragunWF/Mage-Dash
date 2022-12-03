2180f60dba7d92abe5a3ebd6104cd9ad8d9d62f3
Marc Plarisan <plarisanmarc@gmail.com>
2022-12-03 17:17:01 +0800

    Updated GameUI.cs - Implemented powerup UI support

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -9,11 +9,42 @@ public sealed class GameUI : MonoBehaviour
 {
     private TextMeshProUGUI scoreText;
     private TextMeshProUGUI difficultyText;
+    private TextMeshProUGUI powerupText;
 
     private Slider healthSlider;
     private Slider manaSlider;
 
     private DifficultyScaling difficulty;
+
+    private Dictionary<string, string> potionDescriptions = new Dictionary<string, string>();
+    private List<string> activePowerups = new List<string>();
+
+    public void ModifyPowerups(string type, bool addItem)
+    {
+        powerupText.text = "Active Powerups:\n";
+        Debug.Log("Inside modify powerups method");
+
+        if (addItem)
+        {
+            activePowerups.Add(type);
+        }
+        else
+        {
+            activePowerups.Remove(type);
+        }
+
+        if (activePowerups.Count > 0)
+        {
+            foreach (string powerup in activePowerups)
+            {
+                powerupText.text += string.Format("- {0}\n", potionDescriptions[powerup]);
+            }
+        }
+        else
+        {
+            powerupText.text += "- None\n";
+        }
+    }
 
     public static string FormatScore(int points)
     {
@@ -73,16 +104,22 @@ public sealed class GameUI : MonoBehaviour
     {
         scoreText = GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>();
         difficultyText = GameObject.Find("DifficultyText").GetComponent<TextMeshProUGUI>();
-        
+        powerupText = GameObject.Find("PowerupText").GetComponent<TextMeshProUGUI>();
+
         healthSlider = GameObject.Find("HealthSlider").GetComponent<Slider>();
         manaSlider = GameObject.Find("ManaSlider").GetComponent<Slider>();
 
         difficulty = FindObjectOfType<DifficultyScaling>();
+
+        potionDescriptions.Add("mana", "Increased Mana Regen & Capacity");
+        potionDescriptions.Add("score", "Double Score");
+        potionDescriptions.Add("damage", "Double Damage");
     }
 
     private void Start()
     {
         scoreText.text = "Score: 0";
         difficultyText.text = "Difficulty: 1";
+        powerupText.text = "Active Powerups:\n- None";
     }
 }
