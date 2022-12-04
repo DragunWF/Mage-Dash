697a08b13e4929f5616670151a8f780c121c1217
Marc Plarisan <plarisanmarc@gmail.com>
2022-12-04 17:24:23 +0800

    Updated CoinSpawner.cs

--- Assets/Scripts/CoinSpawner.cs
+++ Assets/Scripts/CoinSpawner.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public sealed class CoinSpawner : MonoBehaviour
 {
-    private void Start()
-    {
+    private const float baseSpawnInterval = 17.5f;
+    private float modifiedInterval;
 
+    private GameObject coin;
+    private Vector2[] spawnPositions;
+    private DifficultyScaling difficulty;
+
+    private void Awake()
+    {
+        coin = Resources.Load("Prefabs/Coin") as GameObject;
+        spawnPositions = new Vector2[2] {
+            GameObject.Find("Item Point 1").transform.position,
+            GameObject.Find("Item Point 2").transform.position,
+        };
+        difficulty = FindObjectOfType<DifficultyScaling>();
     }
 
-    private void Update()
+    private void Start()
     {
+        StartCoroutine(SpawnCoins());
+    }
 
+    private IEnumerator SpawnCoins()
+    {
+        const float spawnDelay = 1.5f;
+        yield return new WaitForSeconds(spawnDelay);
+
+        while (true)
+        {
+            yield return new WaitForSeconds(modifiedInterval);
+            // add implementation
+        }
     }
 }
