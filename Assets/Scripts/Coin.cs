12ee3ae9f40bacf06d96d6f0ca00e237ae6ce5bf
Marc Plarisan <plarisanmarc@gmail.com>
2022-12-04 15:18:56 +0800

    Updated Coin.cs - Implemented main functionality

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public sealed class Coin : MonoBehaviour
 {
-    private void Start()
+    private const float baseSpeed = 1.5f;
+    private const float despawnPositionX = -8f;
+    private float modifiedSpeed;
+
+    private void Awake()
     {
-        
+        int level = FindObjectOfType<DifficultyScaling>().DifficultyLevel;
+        modifiedSpeed = baseSpeed + 0.05f * level;
     }
 
     private void Update()
     {
-        
+        float deltaSpeed = modifiedSpeed * Time.deltaTime;
+        transform.Translate(-deltaSpeed, 0, 0);
+
+        if (transform.position.x <= despawnPositionX)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            FindObjectOfType<GameStats>().CollectCoin();
+            FindObjectOfType<AudioPlayer>().PlayPickup();
+            Destroy(gameObject);
+        }
     }
 }
