e8774600c77962a7ce91650e5536617710e2ae0c
Marc Plarisan <plarisanmarc@gmail.com>
2022-12-14 17:43:11 +0800

    Updated ShopMenuUI

--- Assets/Scripts/ShopMenuUI.cs
+++ Assets/Scripts/ShopMenuUI.cs
@@ -7,8 +7,10 @@ using TMPro;
 public sealed class ShopMenuUI : MonoBehaviour
 {
     private Dictionary<string, int> prices = new Dictionary<string, int>();
+    private Dictionary<string, int> statLevels = new Dictionary<string, int>();
     private Dictionary<string, TextMeshProUGUI> shopItems = new Dictionary<string, TextMeshProUGUI>();
     private TextMeshProUGUI coinText;
+    private bool initilizedLevels = false;
 
     private GameStats gameStats;
     private AudioPlayer audioPlayer;
@@ -20,6 +22,19 @@ public sealed class ShopMenuUI : MonoBehaviour
     public void UpgradeSpell() { Upgrade("spell"); }
 
     #endregion
+
+    public void UpdateLevelText(string type)
+    {
+        foreach (KeyValuePair<string, TextMeshProUGUI> pair in shopItems)
+        {
+            if (type == pair.Key)
+            {
+                InitilizeLevels();
+                shopItems[type].text = string.Format("{0} {1}", pair.Key, statLevels[type]);
+                break;
+            }
+        }
+    }
 
     public void UpdateCoinText()
     {
@@ -49,5 +64,34 @@ public sealed class ShopMenuUI : MonoBehaviour
     {
         audioPlayer.PlayUpgrade();
         gameStats.UpgradeStat(stat);
+    }
+
+    private void InitilizeLevels()
+    {
+        if (!initilizedLevels)
+        {
+            statLevels.Add("mana", gameStats.ManaLevel);
+            statLevels.Add("health", gameStats.HealthLevel);
+            statLevels.Add("damage", gameStats.SpellLevel);
+            initilizedLevels = true;
+        }
+        else
+        {
+            foreach (KeyValuePair<string, int> pair in statLevels)
+            {
+                switch (pair.Key)
+                {
+                    case "mana":
+                        statLevels[pair.Key] = gameStats.ManaLevel;
+                        break;
+                    case "health":
+                        statLevels[pair.Key] = gameStats.HealthLevel;
+                        break;
+                    case "damage":
+                        statLevels[pair.Key] = gameStats.SpellLevel;
+                        break;
+                }
+            }
+        }
     }
 }
