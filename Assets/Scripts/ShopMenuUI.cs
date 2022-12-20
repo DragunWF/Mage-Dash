182b6f079bd15ad330250aaa41dab0d2908feb89
DragunWF <plarisanmarc@gmail.com>
2022-12-20 21:24:58 +0800

    Updated ShopMenuUI.cs - Implemented dynamic prompt text on button hovers

--- Assets/Scripts/ShopMenuUI.cs
+++ Assets/Scripts/ShopMenuUI.cs
@@ -10,6 +10,7 @@ public sealed class ShopMenuUI : MonoBehaviour
     private Dictionary<string, int> statLevels = new Dictionary<string, int>();
     private Dictionary<string, TextMeshProUGUI> shopItems = new Dictionary<string, TextMeshProUGUI>();
 
+    private TextMeshProUGUI promptText;
     private TextMeshProUGUI coinText;
     private bool initializedLevels = false;
 
@@ -21,6 +22,15 @@ public sealed class ShopMenuUI : MonoBehaviour
     public void UpgradeMana() { Upgrade("mana"); }
     public void UpgradeHealth() { Upgrade("health"); }
     public void UpgradeSpell() { Upgrade("spell"); }
+
+    #endregion
+
+    #region Hover Methods
+
+    public void OnSpellButtonHover() { OnButtonHover("spell"); }
+    public void OnHealthButtonHover() { OnButtonHover("health"); }
+    public void OnManaButtonHover() { OnButtonHover("mana"); }
+    public void OnButtonExit() { promptText.text = ""; }
 
     #endregion
 
@@ -42,9 +52,12 @@ public sealed class ShopMenuUI : MonoBehaviour
         gameStats = FindObjectOfType<GameStats>();
         audioPlayer = FindObjectOfType<AudioPlayer>();
 
+        coinText = GameObject.Find("CoinText").GetComponent<TextMeshProUGUI>();
+        promptText = GameObject.Find("PromptText").GetComponent<TextMeshProUGUI>();
+
         shopItems.Add("mana", GameObject.Find("ManaText").GetComponent<TextMeshProUGUI>());
         shopItems.Add("health", GameObject.Find("HealthText").GetComponent<TextMeshProUGUI>());
-        shopItems.Add("damage", GameObject.Find("DamageText").GetComponent<TextMeshProUGUI>());
+        shopItems.Add("spell", GameObject.Find("SpellText").GetComponent<TextMeshProUGUI>());
 
         prices.Add("mana", 15);
         prices.Add("health", 15);
@@ -54,6 +67,7 @@ public sealed class ShopMenuUI : MonoBehaviour
     private void Start()
     {
         coinText.text = string.Format("Coin Amount: {0}", gameStats.Coins);
+        promptText.text = "";
 
         foreach (KeyValuePair<string, int> pair in prices)
         {
@@ -91,12 +105,19 @@ public sealed class ShopMenuUI : MonoBehaviour
                     case "health":
                         statLevels[pair.Key] = gameStats.HealthLevel;
                         break;
-                    case "damage":
+                    case "spell":
                         statLevels[pair.Key] = gameStats.SpellLevel;
                         break;
                 }
             }
         }
+    }
+
+    private void OnButtonHover(string buttonType)
+    {
+        int price = prices[buttonType];
+        string coinWord = price > 1 ? "coins" : "coin";
+        promptText.text = string.Format("Price: {0} {1}", price, coinWord);
     }
 
     private string Capitalize(string original)
