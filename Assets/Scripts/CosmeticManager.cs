afccc25e9fc748cdca487ce5692e8b47e11bc178
DragunWF <plarisanmarc@gmail.com>
2022-12-21 19:32:57 +0800

    Updated CosmeticManager.cs - Implemented resource loading

--- Assets/Scripts/CosmeticManager.cs
+++ Assets/Scripts/CosmeticManager.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public sealed class CosmeticManager : MonoBehaviour
 {
+    private Dictionary<string, bool> equippedCosmetics = new Dictionary<string, bool>();
+    private Dictionary<string, RuntimeAnimatorController> cosmetics = new Dictionary<string, RuntimeAnimatorController>();
     private static CosmeticManager instance;
 
     private void Awake()
     {
         ManageSingleton();
+
+        string[] cosmeticNames = { "mage", "ghost", "arthur" };
+        foreach (string name in cosmeticNames)
+        {
+            equippedCosmetics.Add(name, name == "mage"); // Set default outfit as true
+            string formattedPath = string.Format("Animations/Player{0}", Utils.Capitalize(name));
+            cosmetics.Add(name, Resources.Load(formattedPath) as RuntimeAnimatorController);
+        }
     }
 
     private void ManageSingleton()
