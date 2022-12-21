b7268eac162ad4546e7195e491dbbdae2f02a46b
DragunWF <plarisanmarc@gmail.com>
2022-12-21 18:15:39 +0800

    Updated Utils.cs - Moved static methods from other classes

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,16 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public sealed class Utils : MonoBehaviour
 {
-    private void Start()
+    public static string FormatNumber(int points)
     {
+        if (points < 1000)
+        {
+            return points.ToString();
+        }
 
+        string formatted = "", str = points.ToString();
+        for (int i = 1, n = str.Length; i <= n; i++)
+        {
+            formatted += str[str.Length - i];
+            if (i + 1 <= n && i % 3 == 0)
+            {
+                formatted += ",";
+            }
+        }
+
+        char[] charArr = formatted.ToCharArray();
+        Array.Reverse(charArr);
+
+        return new string(charArr);
     }
 
-    private void Update()
+    public static string Capitalize(string original)
     {
-
+        char firstLetter = (char)((int)original[0] - 32);
+        string remaining = original.Substring(1);
+        return string.Format("{0}{1}", firstLetter, remaining);
     }
 }
