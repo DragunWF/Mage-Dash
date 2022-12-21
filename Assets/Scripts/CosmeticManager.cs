a8466edc34b719a3681bbdbd695293247f59ad0a
DragunWF <plarisanmarc@gmail.com>
2022-12-21 18:00:59 +0800

    Added CosmeticManager.cs

---
+++ Assets/Scripts/CosmeticManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class CosmeticManager : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
