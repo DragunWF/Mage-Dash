ffab6c1f711f5821a58fce40491a0f5733d3344e
DragunWF <plarisanmarc@gmail.com>
2022-12-21 18:11:26 +0800

    Added Utils.cs

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class Utils : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
