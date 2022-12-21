ff30b960b899c7e378c8fa157ba653f07e22cdca
DragunWF <plarisanmarc@gmail.com>
2022-12-21 20:49:44 +0800

    Updated CosmeticManager.cs

--- Assets/Scripts/CosmeticManager.cs
+++ Assets/Scripts/CosmeticManager.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public sealed class CosmeticManager : MonoBehaviour
 {
     public RuntimeAnimatorController EquippedCosmetic { get; private set; }
+    public Dictionary<string, bool> ownedCosmetics = new Dictionary<string, bool>();
 
-    private Dictionary<string, bool> equippedCosmetics = new Dictionary<string, bool>();
     private Dictionary<string, RuntimeAnimatorController> cosmetics = new Dictionary<string, RuntimeAnimatorController>();
     private static CosmeticManager instance;
 
     public void ChangeCosmetic(string cosmeticName)
     {
         EquippedCosmetic = cosmetics[cosmeticName];
-        equippedCosmetics[cosmeticName] = true;
+    }
+
+    public string[] GetCosmeticNames()
+    {
+        string[] cosmeticNames = new string[cosmetics.Count];
+        int index = 0;
+        foreach (string key in cosmetics.Keys)
+        {
+            cosmeticNames[index] = key;
+            index++;
+        }
+        return cosmeticNames;
     }
 
     private void Awake()
@@ -23,7 +34,7 @@ public sealed class CosmeticManager : MonoBehaviour
         string[] cosmeticNames = { "mage", "ghost", "arthur" };
         foreach (string name in cosmeticNames)
         {
-            equippedCosmetics.Add(name, name == "mage"); // Set default outfit as true
+            ownedCosmetics.Add(name, name == "mage"); // Set default outfit as true
             string formattedPath = string.Format("Animations/Player{0}", Utils.Capitalize(name));
             cosmetics.Add(name, Resources.Load(formattedPath) as RuntimeAnimatorController);
         }
